65fb8dc90494e77f29453684800f5c870a7ed7b4
David <a00834548@tec.mx>
2023-04-22 18:12:00 -0600

    LoginForm

---
+++ arca-app/src/components/global/LoginForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "../../styles/login.css"
+
+function LoginForm() {
+
+  
+    return (
+      <div className="component">
+        
+      </div>
+    );
+}
+
+export default LoginForm
--- arca-app/src/views/global/LogIn.js
+++ arca-app/src/views/global/LogIn.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import "../../styles/login.css"
+import LoginForm from '../../components/global/LoginForm'
 
 function LogIn() {
   return (
-    <div>LogIn</div>
+    <div className='body'>
+        <LoginForm/>
+    </div>
   )
 }
 
 export default LogIn
+
+
+
